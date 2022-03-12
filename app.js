68741da152afe3e94322939e63493c4a2294448f
Joshua Wallace <jwalla74@uncc.edu>
2022-03-12 18:29:04 -0500

    Run on local host

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/", (req,res) =>{
+	res.render("home");
+})
+app.listen(3000, "localhost", ()=>{
+    console.log("Server is running on 3000");
+});

ae27099c0c311ad69e74bd5f8b469cef57cde869
Joshua Wallace <jwalla74@uncc.edu>
2022-03-17 11:06:46 -0400

    Added locus routes

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req,res) =>{
-	res.render("home");
+	res.render("travel");
 })
+
+app.get("/travel", (req,res) =>{
+	res.render("travel");
+})
+
+app.get("/locus", (req,res) =>{
+	res.render("locus");
+})
+
+app.post("/locus", (req,res) =>{
+	console.log("test")
+})
+
 app.listen(3000, "localhost", ()=>{
     console.log("Server is running on 3000");
 });

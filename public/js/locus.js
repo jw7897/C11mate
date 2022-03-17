ce482856289649510e16fd954349ba4e28034748
Joshua Wallace <jwalla74@uncc.edu>
2022-03-17 15:10:47 -0400

    Further implemented API

--- public/js/locus.js
+++ public/js/locus.js
@@ -1,9 +1,35 @@
 const apiKey = "af9e6368dcce66f0f1162a446b5e0d9a";
-window.onload = function() {
-    document.getElementById("submit").onclick = test;
-  }
+let content = document.getElementById("content").innerHTML;
+let currTemp;
+let feelsLike;
+let pChance;
+let wSpeed;
+let humid;
+let city;
 
-  function test() {
-    let zip = document.getElementById("dLoc").value;
-    const url = 'https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}'
-  };
+window.onload = function() {
+  document.getElementById("submit").onclick = execute;
+}
+
+function execute() {
+  console.log("test");
+  let zip = document.getElementById("dLoc").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      currTemp = (((res.main.temp) - 273.15)*1.8) + 32;
+      feelsLike = (((res.main.feels_like) - 273.15)*1.8) + 32;
+      wSpeed = res.wind.speed;
+      humid = res.main.humidity;
+      city = res.name
+      document.getElementById("content").innerHTML = content + "<br>" + city + "<br> Current Temp: " + currTemp+ "<br> Feels Like: "  + feelsLike+ "<br> Wind Speed: "  + wSpeed+ "<br> Humidity: "  + humid;
+    })
+    .catch((err) => {
+      alert(err);
+    });
+    return;
+};
+
